cbbee7b9baeb8d5cd7063552726ef49f18777f67
Mohid Basha <mohid_basha@icloud.com>
2024-11-03 01:09:13 -0500

    Added Pages for Profile View, Page for Profile Edit, andadded functionality for the saving editing and viewing

---
+++ ProfileViewSave.js
@@ -0,0 +1,36 @@
+window.onload = main; 
+function main() {
+    if (document.getElementById("SaveButton")) {
+        document.getElementById("SaveButton").addEventListener("click", saveInfo);
+    }
+
+    if (document.getElementById("userheightdisplay")) {
+        displayInfo();
+    }
+}
+
+function saveInfo() {
+    let usrheight = document.getElementById("userheight").value;
+    let usrweight = document.getElementById("userweight").value;
+    let usrexperienceLevel = document.getElementById("experienceLevel").value;
+    let usrnotes = document.getElementById("userNotes").value;
+
+    sessionStorage.setItem("userHeight", usrheight);
+    sessionStorage.setItem("userWeight", usrweight);
+    sessionStorage.setItem("experienceLevel", usrexperienceLevel);
+    sessionStorage.setItem("userNotes", usrnotes);
+    alert("Profile Info Saved");
+}
+
+function displayInfo() {
+    let usrheight = sessionStorage.getItem("userHeight");
+    let usrweight = sessionStorage.getItem("userWeight");
+    let usrexperienceLevel = sessionStorage.getItem("experienceLevel");
+    let usrnotes = sessionStorage.getItem("userNotes");
+
+    document.getElementById("userheightdisplay").textContent = usrheight;
+    document.getElementById("userweightdisplay").textContent = usrweight;
+    document.getElementById("userexperienceleveldisplay").textContent = usrexperienceLevel;
+    document.getElementById("usernotesdisplay").textContent = usrnotes;
+    
+}
